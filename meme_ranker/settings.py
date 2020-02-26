1c225f8c6da8fbeb2d59562c76dae957f711e222
0xecho <mr.m.r.s.h.i.e.l.d@gmail.com>
2020-02-26 11:25:19 +0000

    changed media_settings to relative path

--- meme_ranker/settings.py
+++ meme_ranker/settings.py
@@ -126,5 +126,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-MEDIA_ROOT = '/home/csec/temp/meme_ranker/meme_ranker/'
+# MEDIA_ROOT = '/home/csec/temp/meme_ranker/meme_ranker/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'')
 MEDIA_URL = '/memes/'
